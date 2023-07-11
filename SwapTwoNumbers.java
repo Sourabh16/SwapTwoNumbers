92d6fa0435aaa6fc31189c51beb49a6e3e49c111
SOURABH SINGH <singh16sourabh@gmail.com>
2023-07-11 16:35:49 +0530

    added an example

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -12,3 +12,15 @@ public class SwapTwoNumbers {
         
 }
 
+public class SwapTwoNumbers {
+    public static void main(String[ ] args){
+        int a = 20;   /initializing variables
+        int b = 30;
+        
+        int temp = 0;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
